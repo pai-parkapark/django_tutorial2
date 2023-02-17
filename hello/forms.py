fada44a227804d8de2255d0397b18324194505ec
pai-parkapark <tlcnrrn21@publicai.co.kr>
2023-02-17 15:56:04 +0900

    upload form 추가

--- hello/forms.py
+++ hello/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Post
+from .models import Post, Upload
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('author', 'title', 'text',)
+
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Upload
+        fields = ["file"]
