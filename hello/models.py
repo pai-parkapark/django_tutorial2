d15851ed9ae92b5a9e5afe49dc7c11bd5c78ad63
pai-parkapark <tlcnrrn21@publicai.co.kr>
2023-02-17 09:13:10 +0900

    blog 게시물 위한 model 추가

---
+++ hello/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
